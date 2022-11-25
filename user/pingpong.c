75e395e5b767098fbf2472fc3df1cc34603b09db
Linde7777 <69742577+Linde7777@users.noreply.github.com>
2022-11-25 13:03:48 +0000

    add pipe

--- user/pingpong.c
+++ user/pingpong.c
@@ -6,24 +6,26 @@
 int
 main(){
   char arr[]="abc";
+  int fd[2];
+  pipe(fd);
   if(fork()==0){
     char* child_buffer=(char*)malloc(sizeof(char)*(LEN+1));
-    close(0);
-    read(0,child_buffer,LEN);
+    close(fd[0]);
+    read(fd[0],child_buffer,LEN);
     fprintf(2,"%d: received ping\n",getpid());
 
-    close(0);
-    close(1);
-    write(1,child_buffer,LEN);
+    close(fd[0]);
+    close(fd[1]);
+    write(fd[1],child_buffer,LEN);
     exit(0);
   }else{
-    close(1);
-    write(1,arr,LEN);
+    close(fd[1]);
+    write(fd[1],arr,LEN);
     wait(0);
 
     char* parent_buffer=(char*)malloc(sizeof(char)*(LEN+1));
-    close(0);
-    read(0,parent_buffer,LEN);
+    close(fd[0]);
+    read(fd[0],parent_buffer,LEN);
     fprintf(2,"%d: received pong\n",getpid());
     exit(0);
   }
