05f1004e9d531d666e4bfcc44a082bf4002c67c3
Linde7777 <69742577+Linde7777@users.noreply.github.com>
2022-11-25 13:05:04 +0000

    finished

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,30 +3,28 @@
 #include "user/user.h"
 #define LEN 4
 
-int
-main(){
-  char arr[]="abc";
+int main() {
+  char arr[] = "abc";
   int fd[2];
   pipe(fd);
-  if(fork()==0){
-    char* child_buffer=(char*)malloc(sizeof(char)*(LEN+1));
-    close(fd[0]);
-    read(fd[0],child_buffer,LEN);
-    fprintf(2,"%d: received ping\n",getpid());
+  if (fork() == 0) {
+    char *child_buffer = (char *)malloc(sizeof(char) * (LEN + 1));
+    close(fd[1]);
+    read(fd[0], child_buffer, LEN);
+    fprintf(2, "%d: received ping\n", getpid());
 
     close(fd[0]);
-    close(fd[1]);
-    write(fd[1],child_buffer,LEN);
+    write(fd[1], child_buffer, LEN);
     exit(0);
-  }else{
-    close(fd[1]);
-    write(fd[1],arr,LEN);
+  } else {
+    close(fd[0]);
+    write(fd[1], arr, LEN);
     wait(0);
 
-    char* parent_buffer=(char*)malloc(sizeof(char)*(LEN+1));
-    close(fd[0]);
-    read(fd[0],parent_buffer,LEN);
-    fprintf(2,"%d: received pong\n",getpid());
+    char *parent_buffer = (char *)malloc(sizeof(char) * (LEN + 1));
+    close(fd[1]);
+    read(fd[0], parent_buffer, LEN);
+    fprintf(2, "%d: received pong\n", getpid());
     exit(0);
   }
 
