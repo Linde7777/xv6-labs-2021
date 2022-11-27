dad3e301e34bc8a1648c252a544970366e004c7b
Linde7777 <69742577+Linde7777@users.noreply.github.com>
2022-11-27 06:25:15 +0000

    finished

--- user/primes.c
+++ user/primes.c
@@ -1,5 +1,5 @@
-#include "kernel/stat.h"
 #include "kernel/types.h"
+#include "kernel/stat.h"
 #include "user/user.h"
 
 void new_proc(int fd[2]);
@@ -13,20 +13,27 @@ int main() {
   } else {
     for (int i = 2; i <= 35; i++) {
       close(fd[0]);
-      write(fd[1], &i, sizeof(int));
+      if (write(fd[1], &i, sizeof(int)) != sizeof(int)) {
+        //fprintf(2, "write error in main()\n");
+        exit(1);
+      }
     }
-
+    close(fd[1]);
     wait(0);
     exit(0);
   }
 
-  exit(0);
+  return 0;
 }
 
 void new_proc(int fd[2]) {
   int p;
   close(fd[1]);
-  read(fd[0], &p, sizeof(int));
+
+  if(read(fd[0], &p, sizeof(int))!=sizeof(int)){
+    //fprintf(2,"read error in new_proc()\n");
+    exit(1);
+  }
   fprintf(2, "prime %d\n", p);
 
   int next_fd[2];
@@ -39,11 +46,17 @@ void new_proc(int fd[2]) {
     while (read(fd[0], &n, sizeof(int) != 0)) {
       if (n % p != 0) {
         close(next_fd[0]);
-        write(next_fd[1], &n, sizeof(int));
+
+        if(write(next_fd[1], &n, sizeof(int))!=sizeof(int)){
+          //fprintf(2,"write error in new_proc()\n");
+          exit(1);
+        }
       }
     }
+    close(fd[0]);
+    close(next_fd[1]);
     wait(0);
-    exit(0);
   }
 
+  exit(0);
 }
