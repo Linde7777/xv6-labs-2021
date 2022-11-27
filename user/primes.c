7bf70919f1f44e9d6cc7d272d4bc8b7e69567762
Linde7777 <69742577+Linde7777@users.noreply.github.com>
2022-11-27 05:43:39 +0000

    v2

--- user/primes.c
+++ user/primes.c
@@ -1,23 +1,49 @@
-#include "kernel/types.h"
 #include "kernel/stat.h"
+#include "kernel/types.h"
 #include "user/user.h"
+
+void new_proc(int fd[2]);
 
 int main() {
   int fd[2];
   pipe(fd);
+
   if (fork() == 0) {
-    int received_num;
-    close(fd[1]);
-    read(fd[0], &received_num, sizeof(int));
-    fprintf(2, "receive num %d\n", received_num);
-    exit(0);
+    new_proc(fd);
   } else {
-    int num = 3;
-    close(fd[0]);
-    write(fd[1], &num, sizeof(int));
+    for (int i = 2; i <= 35; i++) {
+      close(fd[0]);
+      write(fd[1], &i, sizeof(int));
+    }
+
     wait(0);
     exit(0);
   }
 
   exit(0);
+}
+
+void new_proc(int fd[2]) {
+  int p;
+  close(fd[1]);
+  read(fd[0], &p, sizeof(int));
+  fprintf(2, "prime %d\n", p);
+
+  int next_fd[2];
+  pipe(next_fd);
+  int n;
+  if (fork() == 0) {
+    new_proc(next_fd);
+  } else {
+
+    while (read(fd[0], &n, sizeof(int) != 0)) {
+      if (n % p != 0) {
+        close(next_fd[0]);
+        write(next_fd[1], &n, sizeof(int));
+      }
+    }
+    wait(0);
+    exit(0);
+  }
+
 }
