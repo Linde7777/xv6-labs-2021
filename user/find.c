7d6edf71d2884760817f0d2d560e0e4a8f4f4e7f
Linde7777 <69742577+Linde7777@users.noreply.github.com>
2022-12-21 08:26:00 +0000

    recursion works

--- user/find.c
+++ user/find.c
@@ -1,10 +1,7 @@
 #include "kernel/types.h"
+#include "kernel/fs.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include "kernel/fs.h"
-#include <string.h>
-
-// code are copied from ls.c and be adapted
 
 char *fmtname(char *path) {
   static char buf[DIRSIZ + 1];
@@ -30,42 +27,60 @@ void find(char *filepath, char *filename) {
   struct stat st;
 
   if ((fd = open(filepath, 0)) < 0) {
-    fprintf(2, "find: cannot open %s\n", filepath);
+    fprintf(2, "ls: cannot open %s\n", filepath);
     return;
   }
 
   if (fstat(fd, &st) < 0) {
-    fprintf(2, "find: cannot stat %s\n", filepath);
+    fprintf(2, "ls: cannot stat %s\n", filepath);
     close(fd);
     return;
   }
 
   switch (st.type) {
   case T_FILE:
-    char *curr_filename = fmtname(filepath);
-    if(strcmp(*curr_filename,filename)==0){
-      printf("%s %d %d %l\n", *curr_filename, st.type, st.ino, st.size);
-    }
+    //printf("Meet File: %s ",filepath);
+    printf("%s %d %d %l\n", fmtname(filepath), st.type, st.ino, st.size);
     break;
 
   case T_DIR:
+    //printf("Meet Dir: %s ",filepath);
     if (strlen(filepath) + 1 + DIRSIZ + 1 > sizeof buf) {
-      printf("find: path too long\n");
+      printf("ls: path too long\n");
       break;
     }
     strcpy(buf, filepath);
     p = buf + strlen(buf);
     *p++ = '/';
     while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+      if(de.name[0]=='.'){
+        continue;
+      }
       if (de.inum == 0)
         continue;
       memmove(p, de.name, DIRSIZ);
       p[DIRSIZ] = 0;
       if (stat(buf, &st) < 0) {
-        printf("find: cannot stat %s\n", buf);
+        printf("ls: cannot stat %s\n", buf);
         continue;
       }
-      printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+
+      //printf("name:%s, type:%d",de.name,st.type);
+      //printf("Enter Dir: %s ",filepath);
+      //printf("buf: %s ", buf);
+      //printf("filepath: %s ",filepath);
+      //printf("\n");
+      if(st.type==1){
+        find(de.name,filename);
+      }else{
+        if (strcmp(de.name, filename) == 0) {
+          //printf("FIND IT!!!\n");
+          printf("%s %d %d %d\n", de.name, st.type, st.ino, st.size);
+        }
+      }
+      
+    
+    
     }
     break;
   }
@@ -73,8 +88,6 @@ void find(char *filepath, char *filename) {
 }
 
 int main(int argc, char *argv[]) {
-  int i;
-
   if (argc < 3) {
     fprintf(2, "Usage: find filepath filename\n");
     exit(0);
