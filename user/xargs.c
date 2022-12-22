a4e12149294e5ae1a518d8992e6aa0f8ca803a14
Linde7777 <69742577+Linde7777@users.noreply.github.com>
2022-12-22 14:04:54 +0000

    v2

--- user/xargs.c
+++ user/xargs.c
@@ -1,12 +1,42 @@
-#include <stdio.h>
+#include "kernel/types.h"
+#include "kernel/stat.h"
 #include "user/user.h"
+#define ARGUMENT_LEN 20
 
-void xargs() {}
+char *concat(char *s1, char *s2) {
+  int len1 = strlen(s1);
+  int len2 = strlen(s2);
+  int len3 = len1 + len2;
+  char *s3 = (char *)malloc(sizeof(char) * len3);
 
-void get_input() {
-  int c;
-  while ((c=getchar())!=EOF) {
-    
+  char *p1 = s1;
+  char *p2 = s2;
+  char *p3 = s3;
+  while (*p1 != '\0') {
+    *p3 = *p1;
+  }
+  while (*p2 != '\0') {
+    *p3 = *p2;
+  }
+  p3 += 1;
+  *p3 = '\0';
+
+  return s3;
+}
+
+void xargs(char *program_name, char *argument1) {
+  // TODO: free it
+  char *argument2 = (char *)malloc(sizeof(char) * ARGUMENT_LEN);
+  while (gets(argument2, ARGUMENT_LEN)) {
+    if (fork() == 0) {
+      printf("enter child process\n");
+      char *argv[1];
+      argv[0] = concat(argument1, argument2);
+      exec(program_name, argv);
+    } else {
+      wait(0);
+      printf("wait completed, enter parent process\n");
+    }
   }
 }
 
@@ -17,7 +47,11 @@ int main(int argc, char *argv[]) {
   }
 
   char *program_name = argv[1];
+  char *argument1 = "";
   if (argc == 3) {
-    char *argument_partition = argv[2];
+    argument1 = argv[2];
   }
+
+  xargs(program_name, argument1);
+  exit(0);
 }
