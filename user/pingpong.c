b15647cf58216548ac5abc0fb98ec6a23bd40f1d
Linde7777 <69742577+Linde7777@users.noreply.github.com>
2022-12-26 08:04:41 +0000

    change fprintf into printf

--- user/pingpong.c
+++ user/pingpong.c
@@ -11,7 +11,7 @@ int main() {
     char *child_buffer = (char *)malloc(sizeof(char) * (LEN + 1));
     close(fd[1]);
     read(fd[0], child_buffer, LEN);
-    fprintf(2, "%d: received ping\n", getpid());
+    printf("%d: received ping\n", getpid());
 
     close(fd[0]);
     write(fd[1], child_buffer, LEN);
@@ -25,7 +25,7 @@ int main() {
     char *parent_buffer = (char *)malloc(sizeof(char) * (LEN + 1));
     close(fd[1]);
     read(fd[0], parent_buffer, LEN);
-    fprintf(2, "%d: received pong\n", getpid());
+    printf("%d: received pong\n", getpid());
     free(parent_buffer);
     exit(0);
   }
