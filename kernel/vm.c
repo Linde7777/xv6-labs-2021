6e4ef621132e78b87b0d153cdde3cdccefd9fbc6
Linde7777 <69742577+Linde7777@users.noreply.github.com>
2023-02-05 05:25:40 +0000

    print pagetable v1

--- kernel/vm.c
+++ kernel/vm.c
@@ -281,6 +281,31 @@ freewalk(pagetable_t pagetable)
   kfree((void*)pagetable);
 }
 
+void
+vmprint_helper(pagetable_t pagetable, int depth)
+{
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      for(int i=0;i<=depth;i++){
+        printf("..");
+      }
+      uint64 child = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n",depth, pte, child);
+      vmprint_helper((pagetable_t)child,depth+1);
+      pagetable[i] = 0;
+    } 
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n",pagetable);
+  vmprint_helper(pagetable, 0);
+}
+
 // Free user memory pages,
 // then free page-table pages.
 void
@@ -431,9 +456,4 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   } else {
     return -1;
   }
-}
-
-void
-vmprint(pagetable_t pagetable){
-  
 }
