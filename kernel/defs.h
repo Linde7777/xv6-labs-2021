716d8ac8a869c622f3eac62ebe786be0d671f907
Linde7777 <69742577+Linde7777@users.noreply.github.com>
2023-02-05 05:00:58 +0000

    update

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,6 +63,7 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+int             count_free_mem(void);
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -170,6 +171,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+void            vmprint(pagetable_t pagetable);
 
 // plic.c
 void            plicinit(void);
