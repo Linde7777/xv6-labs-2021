16e4379ba1d46b6d76d5ff460a10c30aaf752119
Linde7777 <69742577+Linde7777@users.noreply.github.com>
2023-02-16 06:55:35 +0000

    modify struct proc for sys_sigalarm

--- kernel/proc.h
+++ kernel/proc.h
@@ -86,6 +86,10 @@ enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 struct proc {
   struct spinlock lock;
 
+  // for sys_sigalarm
+  int interval;
+  void (*handler)();
+
   // p->lock must be held when using these:
   enum procstate state;        // Process state
   void *chan;                  // If non-zero, sleeping on chan
