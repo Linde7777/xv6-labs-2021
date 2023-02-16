6040fac86651c950c7f9707be3e88ec6058683d4
Linde7777 <69742577+Linde7777@users.noreply.github.com>
2023-02-16 06:34:03 +0000

    backtrace() finished

--- user/call.c
+++ user/call.c
@@ -15,3 +15,19 @@ void main(void) {
   printf("%d %d\n", f(8)+1, 13);
   exit(0);
 }
+
+/**
+  1. a0 and a7 contain arguments to functions,
+  in this function, a2 store 13
+  
+  2. idk
+
+  3. 1536
+
+  4. 0x38
+
+  5. He110 World
+
+  6. random number, it is the value in the block 
+  that right after variable 3 located in.
+ */
