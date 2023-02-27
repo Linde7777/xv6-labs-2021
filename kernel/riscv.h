f70e29207de911622e133644092cec21b8de6911
Linde7777 <69742577+Linde7777@users.noreply.github.com>
2023-02-27 07:45:32 +0000

    add more comment

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -343,7 +343,8 @@ sfence_vma()
 #define PTE_W (1L << 2)
 #define PTE_X (1L << 3)
 #define PTE_U (1L << 4) // 1 -> user can access
-#define PTE_A (1L << 6) // google riscv-priviledged, go to Figure 4.15
+#define PTE_A (1L << 6) // go to https://riscv.org/wp-content/uploads/2017/05/riscv-privileged-v1.10.pdf
+                        // figure 4.15 at page 70
 
 // shift a physical address to the right place for a PTE.
 #define PA2PTE(pa) ((((uint64)pa) >> 12) << 10)
